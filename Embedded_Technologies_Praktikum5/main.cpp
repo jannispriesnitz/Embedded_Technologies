3fece7d9cf0854138c4db5dfad48537c257f61d4
jannispriesnitz <jannis-priesnitz@freenet.de>
2014-06-07 20:25:22 +0200

    Initial commit

---
+++ Embedded_Technologies_Praktikum5/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: jannis
+ *
+ * Created on 7. Juni 2014, 20:24
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
